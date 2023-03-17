0b384aeeba868a1610e52acc64390546a166d380
Matt Mullins <matt.mullins@live.com>
2023-03-17 11:20:39 +0000

    Update age function and work with REPL

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,14 @@
 function Pet(name) {
     this.name = name;
-}
+    this.age = 0;
+};
+
+Pet.prototype.growUp = function() {
+  this.age += 1;
+};
+
+const fido = new Pet('Fido');
+const rex = new Pet('Rex');
 
 module.exports = Pet;
+
